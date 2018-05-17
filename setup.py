9e91cc9ebe1432ddbcae36fc779a993e01491882
luvemil <taran.marco@gmail.com>
2018-05-17 12:26:05 +0200

    Add oauth dance

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import pickle
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -5,7 +6,49 @@ from tweepy import OAuthHandler
 
 import os
 auth = OAuthHandler(os.getenv("CONSUMER_KEY"),os.getenv("CONSUMER_SECRET"))
-auth.set_access_token(os.getenv("ACCESS_TOKEN"),os.getenv("ACCESS_TOKEN_SECRET"))
+# auth.set_access_token(os.getenv("ACCESS_TOKEN"),os.getenv("ACCESS_TOKEN_SECRET"))
 
 import tweepy
 api = tweepy.API(auth)
+
+def get_api_token(auth):
+    try:
+        redirect_url = auth.get_authorization_url()
+    except tweepy.TweepError:
+        print('Error! Failed to get request token.')
+        return None
+
+    print("Visit {} and write here the code".format(redirect_url))
+    verifier = raw_input('Verifier:')
+
+    try:
+        auth.get_access_token(verifier)
+    except tweepy.TweepError:
+        print('Error! Failed to get access token.')
+
+    access_data = {
+            "key": auth.access_token,
+            "secret": auth.access_token_secret,
+            "status": "OK"
+            }
+    token_file = open("token.pickle","w")
+    pickle.dump(access_data,token_file)
+    token_file.close()
+    return auth
+
+def setup_api(auth):
+    try:
+        token_file = open("token.pickle","r")
+        access_data = pickle.load(token_file)
+        token_file.close()
+        if access_data["status"] != "OK":
+            raise ValueError("Status is not OK")
+        auth.set_access_token(access_data["key"],access_data["secret"])
+    except ValueError:
+        token_file.close()
+        auth = get_api_token(auth)
+    except FileNotFoundError:
+        auth = get_api_token(auth)
+
+    api = tweepy.API(auth)
+    return api
