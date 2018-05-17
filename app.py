fcd6d8cb9e661497fc878a148151f45d90a349e0
luvemil <taran.marco@gmail.com>
2018-05-17 12:26:16 +0200

    Use oauth dance in app

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import tweepy
 
-from setup import auth,api
+from setup import auth,setup_api
 from handlers import MyStreamListener
+
+api = setup_api(auth)
 
 myStreamListener = MyStreamListener()
 myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
