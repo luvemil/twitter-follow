b1101fe8d20ba8246cd33fa9303bb144de9b2cf3
luvemil <taran.marco@gmail.com>
2018-05-24 16:20:33 +0200

    Test save tweets on disk

--- test_push.py
+++ test_push.py
@@ -3,25 +3,44 @@ import tweepy
 from setup import auth,setup_api
 from handlers import *
 from tbot import bot, updater, groups
-from tools import format_status
+from tools import *
+import telegram
+import pickle
+import codecs
 
 import os
 
 import logging
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
 
 api = setup_api(auth)
 
 sender_id = '52424550' # Il fatto quotidiano
 
+msgs = []
+
 def push_tweet(status):
     if status.author.id == sender_id:
-        msg = "! " + format_status(status)
+        p_status = default_parse_status(status)
+        msg = "*{} @ {}*: {}".format(
+                p_status['author'],
+                p_status['date'],
+                p_status['content']
+                )
     else:
         msg = format_status(status)
+
+    msgs.append(msg)
     for gid in groups:
-        bot.send_message(chat_id=gid,text=msg)
+        try:
+            bot.send_message(
+                    chat_id=gid,
+                    text=msg,
+                    parse_mode=telegram.ParseMode.MARKDOWN
+                    )
+        except telegram.error.BadRequest:
+            logging.info("Error sending message: {}".format(msg))
 
 myStreamListener = CallbackStreamListener(callback=push_tweet)
 myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
@@ -34,4 +53,10 @@ except KeyboardInterrupt:
 finally:
     updater.stop()
     myStream.disconnect()
+    print("Writing tweets to file")
+    outfile = codecs.open("tweets","a","utf-8")
+    for msg in msgs:
+        outfile.write(msg+"\n")
+    outfile.close()
+    print("Tweets, written")
     print("Stream closed. Goodbye!")
