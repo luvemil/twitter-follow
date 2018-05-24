db727c506a6a9604e84e4b805f74b9ea92fb16a9
luvemil <taran.marco@gmail.com>
2018-05-24 17:46:16 +0200

    Add retries to BadRequests, keeps failing

--- callbacks.py
+++ callbacks.py
@@ -2,6 +2,24 @@ import telegram
 import logging
 from tools import default_parse_status, format_status
 
+def send_with_retry(bot,chat_id,msg,trynum=1,maxretry=5):
+    try:
+        bot.send_message(
+                chat_id=chat_id,
+                text=msg,
+                parse_mode=telegram.ParseMode.MARKDOWN
+                )
+    except telegram.error.BadRequest:
+        # TODO: Try resending BadRequests
+        logging.info("BadRequest caught at try {} sending message: {}".format(trynum,msg))
+        if trynum <= maxretry:
+            send_with_retry(bot,chat_id,msg,trynum+1,maxretry)
+        else:
+            logging.info("BadRequest exceeds maxretry for message: {}".format(msg))
+    except telegram.error.TimedOut:
+        # NOTE: When a TimeOut occurs, apparently the message gets sent anyway
+        logging.info("TimeOut caught sending message: {}".format(msg))
+
 def make_push_tweet(bot,author_id_str,targets,msg_q=None):
     sender_id = str(author_id_str)
 
@@ -23,18 +41,7 @@ def make_push_tweet(bot,author_id_str,targets,msg_q=None):
         if msg_q is not None:
             msg_q.append(msg)
         for gid in targets:
-            try:
-                bot.send_message(
-                        chat_id=gid,
-                        text=msg,
-                        parse_mode=telegram.ParseMode.MARKDOWN
-                        )
-            except telegram.error.BadRequest:
-                # TODO: Try resending BadRequests
-                logging.info("BadRequest caught sending message: {}".format(msg))
-            except telegram.error.TimedOut:
-                # NOTE: When a TimeOut occurs, apparently the message gets sent anyway
-                logging.info("TimeOut caught sending message: {}".format(msg))
+            send_with_retry(bot,gid,msg)
 
     return push_tweet
 
