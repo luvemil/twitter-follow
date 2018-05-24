5b77d4a7000de72ff88bb4150f563261c42f29ff
luvemil <taran.marco@gmail.com>
2018-05-24 15:29:00 +0200

    Add function to retrieve user mentions

--- tools.py
+++ tools.py
@@ -1,7 +1,12 @@
+import tweepy
 
 def _get_user_timeline(api,id,count=10):
 # Not really useful
     return api.user_timeline(id=id,count=count)[::-1]
+
+def get_replies_to_user(api,user_handle,since_id=None):
+    search_q = "@{}".format(user_handle)
+    return tweepy.Cursor(api.search,q=search_q,since_id=since_id)
 
 def format_status(status):
     author_name = status.author.screen_name
