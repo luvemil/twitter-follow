a49f040a00fc107115396308673ebaa5fb3088d1
luvemil <taran.marco@gmail.com>
2018-05-24 17:28:46 +0200

    Change formatting for replies

--- callbacks.py
+++ callbacks.py
@@ -6,15 +6,19 @@ def make_push_tweet(bot,author_id_str,targets,msg_q=None):
     sender_id = str(author_id_str)
 
     def push_tweet(status):
+        p_status = default_parse_status(status)
         if status.author.id_str == sender_id:
-            p_status = default_parse_status(status)
-            msg = "*{} @ {}*: {}".format(
+            msg = "*{} @ {}*:\n{}".format(
                     p_status['author'],
                     p_status['date'],
                     p_status['content']
                     )
         else:
-            msg = format_status(status)
+            msg = "-- {} @ {}:\n{}".format(
+                    p_status['author'],
+                    p_status['date'],
+                    p_status['content']
+                    )
 
         if msg_q is not None:
             msg_q.append(msg)
@@ -26,8 +30,10 @@ def make_push_tweet(bot,author_id_str,targets,msg_q=None):
                         parse_mode=telegram.ParseMode.MARKDOWN
                         )
             except telegram.error.BadRequest:
+                # TODO: Try resending BadRequests
                 logging.info("BadRequest caught sending message: {}".format(msg))
             except telegram.error.TimedOut:
+                # NOTE: When a TimeOut occurs, apparently the message gets sent anyway
                 logging.info("TimeOut caught sending message: {}".format(msg))
 
     return push_tweet
