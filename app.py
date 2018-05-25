097671914e76d7520aae7d18ca4294a25bd804c7
luvemil <taran.marco@gmail.com>
2018-05-25 14:00:54 +0200

    Use new listener in main app

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import tweepy
 
 from setup import auth,setup_api
-from handlers import MyStreamListener
+from handlers import CallbackStreamListener
 from tbot import bot, updater, groups
+from callbacks import make_push_tweet
 
 import os
 
@@ -10,15 +11,13 @@ target_id = os.getenv("DEFAULT_TWITTER_TARGET")
 
 import logging
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
 
 api = setup_api(auth)
 
-def push_tweet(msg):
-    for gid in groups:
-        bot.send_message(chat_id=gid,text=msg)
+push_tweet = make_push_tweet(bot,target_id,groups)
 
-myStreamListener = MyStreamListener(callback=push_tweet)
+myStreamListener = CallbackStreamListener(callback=push_tweet)
 myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
 
 try:
